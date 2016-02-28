646a068c8e8603a5e9abca2c0d59956c592135e1
sksamuel <sam@sksamuel.com>
2016-02-28 22:03:53 +0000

    Added 2.12 cross build

--- project/Scoverage.scala
+++ project/Scoverage.scala
@@ -1,33 +1,33 @@
 import sbt.Keys._
 import sbt._
-import sbtrelease.ReleasePlugin
-import sbtrelease.ReleasePlugin.ReleaseKeys
 import com.typesafe.sbt.pgp.PgpKeys
 
 object Scoverage extends Build {
 
   val Org = "org.scoverage"
-  val Scala = "2.11.4"
+  val ScalaVersion = "2.12.0-M3"
   val MockitoVersion = "1.9.5"
-  val ScalatestVersion = "2.2.2"
+  val ScalatestVersion = "3.0.0-M12"
 
   lazy val LocalTest = config("local") extend Test
 
   val appSettings = Seq(
     organization := Org,
-    scalaVersion := Scala,
-    crossScalaVersions := Seq("2.10.4", "2.11.7"),
+    scalaVersion := ScalaVersion,
+    crossScalaVersions := Seq("2.10.6", "2.11.7", ScalaVersion),
     fork in Test := false,
     publishMavenStyle := true,
     publishArtifact in Test := false,
     parallelExecution in Test := false,
+    sbtrelease.ReleasePlugin.autoImport.releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+    sbtrelease.ReleasePlugin.autoImport.releaseCrossBuild := true,
     scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8"),
     resolvers := ("releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2") +: resolvers.value,
     concurrentRestrictions in Global += Tags.limit(Tags.Test, 1),
     javacOptions := Seq("-source", "1.6", "-target", "1.6"),
     libraryDependencies ++= Seq(
-      "org.mockito" % "mockito-all" % MockitoVersion % "test",
-      "org.scalatest" %% "scalatest" % ScalatestVersion % "test"
+      "org.mockito"         % "mockito-all" % MockitoVersion % "test",
+      "org.scalatest"       %% "scalatest" % ScalatestVersion % "test"
     ),
     publishTo <<= version {
       (v: String) =>
@@ -61,9 +61,6 @@ object Scoverage extends Build {
     pomIncludeRepository := {
       _ => false
     }
-  ) ++ ReleasePlugin.releaseSettings ++ Seq(
-    ReleaseKeys.crossBuild := true,
-    ReleaseKeys.publishArtifactsAction := PgpKeys.publishSigned.value
   )
 
   lazy val root = Project("scalac-scoverage", file("."))
@@ -81,18 +78,20 @@ object Scoverage extends Build {
     .settings(name := "scalac-scoverage-plugin")
     .settings(appSettings: _*)
     .settings(libraryDependencies ++= Seq(
-    "org.scala-lang" % "scala-reflect" % scalaVersion.value % "provided",
-    "org.scala-lang" % "scala-compiler" % scalaVersion.value % "provided",
-    "org.joda" % "joda-convert" % "1.6" % "test",
-    "joda-time" % "joda-time" % "2.3" % "test",
-    "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2" % "test"
+    "org.scala-lang"                % "scala-reflect"             % scalaVersion.value    % "provided",
+    "org.scala-lang"                % "scala-compiler"            % scalaVersion.value    % "provided",
+    "org.joda"                      %  "joda-convert"             % "1.6"       % "test",
+    "joda-time"                     %  "joda-time"                % "2.3"       % "test"
   )).settings(libraryDependencies ++= {
     CrossVersion.partialVersion(scalaVersion.value) match {
+      case Some((2, scalaMajor)) if scalaMajor == 12 =>
+        EnvSupport.setEnv("CrossBuildScalaVersion", "2.12.0-M3")
+        Seq("org.scala-lang.modules" % "scala-xml_2.12.0-M3" % "1.0.5")
       case Some((2, scalaMajor)) if scalaMajor == 11 =>
-        EnvSupport.setEnv("CrossBuildScalaVersion", "2.11.4")
-        Seq("org.scala-lang.modules" %% "scala-xml" % "1.0.1")
+        EnvSupport.setEnv("CrossBuildScalaVersion", "2.11.7")
+        Seq("org.scala-lang.modules" % "scala-xml_2.11" % "1.0.5")
       case _ =>
-        EnvSupport.setEnv("CrossBuildScalaVersion", "2.10.4")
+        EnvSupport.setEnv("CrossBuildScalaVersion", "2.10.6")
         Nil
     }
   })
